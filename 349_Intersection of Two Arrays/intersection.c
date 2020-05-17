d4252ff978097448458d6a46d67866b9b97523de
Foggy-moon <pingyuanzhaoyang@outlook.com>
2020-05-17 14:13:03 +0800

    update
    利用两数组都是排序的特性，将判断不等于优化为判断大于、小于，使得大数组值循环一边，平均复杂度优化。

--- 349_Intersection of Two Arrays/intersection.c
+++ 349_Intersection of Two Arrays/intersection.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 int getMiddle(int* nums, int left, int right)
 {
 	int pivot = nums[left];
@@ -23,15 +20,17 @@ void quickSort(int* nums, int left, int right)
 	{
 		int mid = getMiddle(nums, left, right);
 		quickSort(nums, left, mid - 1);
-		quickSort(nums, mid+1, right);
+		quickSort(nums, mid + 1, right);
 	}
 }
-int findSector(int *numslitter, int *numslarge, int littleSize, int largeSize,int *res)
+int findSector(int* numslitter, int* numslarge, int littleSize, int largeSize, int* res)
 {
-	int  i, j=0, k = 0;
+	int  i, j = 0, k = 0;
+	int flag;
 	//双指针法查找共同元素
-	for(i=0;i<= littleSize-1 && j<=largeSize-1;i++)
+	for (i = 0; i <= littleSize - 1 && j <= largeSize - 1; i++)
 	{
+		flag = 1;
 		if (i != 0)
 		{
 			if (numslitter[i] == numslitter[i - 1])
@@ -39,18 +38,24 @@ int findSector(int *numslitter, int *numslarge, int littleSize, int largeSize,in
 		}
 		while (numslitter[i] != numslarge[j])
 		{
-			if (j == largeSize - 1)
+			if (numslarge[j] > numslitter[i])
 			{
-				j++;
+				flag = 0;
 				break;
 			}
-			j++;
+			else
+			{
+				if (j == largeSize - 1)
+				{
+					j++;
+					flag = 0;
+					break;
+				}
+				j++;
+			}
 		}
-		if (j <= largeSize - 1)
-		{
+		if (flag == 1)
 			res[k++] = numslarge[j++];
-		}
-		else
 	}
 	return k;
 
