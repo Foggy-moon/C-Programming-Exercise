395fa1b64388e1a1160a80e5d93866098861c6c6
Foggy-moon <pingyuanzhaoyang@outlook.com>
2020-05-17 14:09:42 +0800

    Update intersection.c
    利用两数组都是排序的特性，将判断不等于优化为判断大于、小于，使得大数组值循环一边，平均复杂度优化。

--- 349_Intersection of Two Arrays/intersection.c
+++ 349_Intersection of Two Arrays/intersection.c
@@ -51,30 +51,18 @@ int findSector(int *numslitter, int *numslarge, int littleSize, int largeSize,in
 			res[k++] = numslarge[j++];
 		}
 		else
-			j=0;
-
 	}
 	return k;
-	
+
 }
 int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
-	quickSort(nums1, 0, nums1Size-1);
+	quickSort(nums1, 0, nums1Size - 1);
 	quickSort(nums2, 0, nums2Size - 1);
 	*returnSize = nums1Size > nums2Size ? nums2Size : nums1Size;
 	int* res = (int*)malloc(sizeof(int) * (*returnSize));
 	if (nums1Size > nums2Size)
-		*returnSize = findSector(nums2, nums1, nums2Size, nums1Size,res);
+		*returnSize = findSector(nums2, nums1, nums2Size, nums1Size, res);
 	else
-		*returnSize = findSector(nums1, nums2, nums1Size, nums2Size,res);
+		*returnSize = findSector(nums1, nums2, nums1Size, nums2Size, res);
 	return res;
 }
-
-void main()
-{
-	int nums1[] = { 4,9,5 };
-	int nums2[] = { 9,4,9,8,4};
-	int returnSize = 1;
-	int* res = intersection(nums1, 3, nums2, 5, &returnSize);
-
-}
-
