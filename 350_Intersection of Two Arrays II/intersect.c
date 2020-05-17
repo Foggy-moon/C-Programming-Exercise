b84bc681fbea23d4941942bcc3444b95a4b48d92
Foggy-moon <pingyuanzhaoyang@outlook.com>
2020-05-17 22:35:58 +0800

    Create intersect.c
    查找两个数组中的重复元素（非去重）

---
+++ 350_Intersection of Two Arrays II/intersect.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getMiddle(int* nums, int left, int right)
+{
+	int pivot = nums[left];
+	while (left < right)
+	{
+		while (nums[right] >= pivot && right > left)
+			right--;
+		nums[left] = nums[right];
+		while (nums[left] <= pivot && right > left)
+			left++;
+		nums[right] = nums[left];
+
+	}
+	nums[left] = pivot;
+	return left;
+}
+void quickSort(int* nums, int left, int right)
+{
+	if (left < right)
+	{
+		int mid = getMiddle(nums, left, right);
+		quickSort(nums, left, mid - 1);
+		quickSort(nums, mid + 1, right);
+	}
+}
+int findSector(int* numslitter, int* numslarge, int littleSize, int largeSize, int* res)
+{
+	int  i, j = 0, k = 0;
+	int flag;
+	//双指针法查找共同元素
+	for (i = 0; i <= littleSize - 1 && j <= largeSize - 1; i++)
+	{
+		flag = 1;
+		while (numslitter[i] != numslarge[j])
+		{
+			if (numslarge[j] > numslitter[i])
+			{
+				flag = 0;
+				break;
+			}
+			else
+			{
+				if (j == largeSize - 1)
+				{
+					j++;
+					flag = 0;
+					break;
+				}
+				j++;
+			}
+		}
+		if (flag == 1)
+		{
+			res[k++] = numslarge[j++];
+		}
+	}
+	return k;
+
+}
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+	quickSort(nums1, 0, nums1Size - 1);
+	quickSort(nums2, 0, nums2Size - 1);
+	*returnSize = nums1Size > nums2Size ? nums2Size : nums1Size;
+	int* res = (int*)malloc(sizeof(int) * (*returnSize));
+	if (nums1Size > nums2Size)
+		*returnSize = findSector(nums2, nums1, nums2Size, nums1Size, res);
+	else
+		*returnSize = findSector(nums1, nums2, nums1Size, nums2Size, res);
+	return res;
+}
+
+
+void main()
+{
+	int nums1[] = { 1,2,2,1 };
+	int nums2[] = { 2,2};
+	int returnSize = 2;
+	int* res = intersect(nums1, 4, nums2, 2, &returnSize);
+
+}
+
