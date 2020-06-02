2000da6c1d7daea82cdaf3de96aaa00c6283a7fd
Foggy-moon <pingyuanzhaoyang@outlook.com>
2020-06-02 23:25:15 +0800

    Create djset.c
    并查集判断是否存在环

---
+++ djset/djset.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#define VERTICES 6
+/*并查集判断是否存在环*/
+void initialize(int parent[]){
+	memset(parent,-1,sizeof(int)*VERTICES);
+}
+
+int find_root(int x, int parent[]){
+	int x_root = x;
+	while(parent[x_root] != -1)
+		x_root = parent[x_root];
+	return x_root;
+}
+
+int union_vertices(int x, int y, int parent[]){
+	int x_root=find(x, parent);
+	int y_root=find(y, parent);
+	if (x_root == y_root)
+		return 0;
+	else{
+		parent[x_root] = y_root;
+		return 1;
+	}
+}
+void main()
+{
+	int parent[VERTICES]={0};
+	initialize(parent);
+	int egdes[][2]={
+		{0,1},{1,2},{1,3},
+		{2,4},{3,4},{2,5}
+	}
+	int i;
+	for(i=0;i<VERTICES;i++){
+		int x=edge[i][0];
+		int y=edge[i][1];
+		if(union_vertices(x,y,parent) == 0){
+			printf("Circle detected");
+			exit(0);
+		}
+	}
+	printf("No circles found");
+}
