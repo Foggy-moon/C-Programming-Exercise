550ebf5ead460253effdc0b35fe8fcdb295b82b1
Foggy-moon <pingyuanzhaoyang@outlook.com>
2020-06-03 22:21:52 +0800

    Update djset.c
    优化，引入根节点rank，减少find_root的查找次数

--- djset/djset.c
+++ djset/djset.c
@@ -7,6 +7,7 @@ void initialize(int parent[]) {
 	memset(parent, -1, sizeof(int) * VERTICES);
 }
 
+//为了减少find_root查找的次数，根节点所在的高度rank要尽可能小
 int find_root(int x, int parent[]) {
 	int x_root = x;
 	while(parent[x_root] != -1)
@@ -14,19 +15,26 @@ int find_root(int x, int parent[]) {
 	return x_root;
 }
 
-int union_vertices(int x, int y, int parent[]) {
+int union_vertices(int x, int y, int parent[], int rank[]) {
 	int x_root = find_root(x, parent);
 	int y_root = find_root(y, parent);
 	if (x_root == y_root)
 		return 0;
-	else {
+	//取rank大的根节点作为合并之后的根节点，可以使得合并后的根节点的rank不变
+	if(rank[x_root] > rank[y_root])
+		parent[y_root] = x_root;
+	else if(rank[x_root] < rank[y_root])
 		parent[x_root] = y_root;
-		return 1;
+	else{
+		parent[x_root] = y_root;
+		rank[y_root]++;
 	}
 }
 void main()
 {
 	int parent[VERTICES] = { 0 };
+	//根节点所在的树的高度，默认为0
+	int rank[VERTICES] = { 0 };
 	initialize(parent);
 	int egdes[][2] = {
 		{0,1},{1,2},{1,3},
@@ -36,7 +44,7 @@ void main()
 	for (i = 0; i < VERTICES; i++) {
 		int x = egdes[i][0];
 		int y = egdes[i][1];
-		if (union_vertices(x, y, parent) == 0) {
+		if (union_vertices(x, y, parent, rank) == 0) {
 			printf("Circle detected\n");
 			exit(0);
 		}
